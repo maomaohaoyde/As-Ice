7fc19446926238be659ba84335d014359be2eb42
maomaohaoyde <maomaohaoyde@hotmail.com>
2016-01-23 23:31:42 -0800

    o

---
+++ As Ice/calAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  calAppDelegate.h
+//  As Ice
+//
+//  Created by Charles on 13-8-9.
+//  Copyright (c) 2013年 Charles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface calAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ As Ice/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  As Ice
+//
+//  Created by Charles on 13-8-9.
+//  Copyright (c) 2013年 Charles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "calAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([calAppDelegate class]));
+    }
+}
